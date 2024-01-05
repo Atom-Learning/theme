49ac9467b82918dbe50b089d49ed4dba0157ff9a
Thomas Digby <tom@atomlearning.co.uk>
2024-01-05 17:54:32 +0000

    feat: wip quest theme

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,15 @@
-const config = require('../style.config')
-const StyleDictionary = require('style-dictionary').extend(config)
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
 const formatterSystemUiThemeTypes = require('../src/formatters/system-ui-theme-types')
 const formatterSystemUiTheme = require('../src/formatters/system-ui-theme')
 const formatterScssMapFlat = require('../src/formatters/scss-map-flat')
 const actionMergeFiles = require('../src/actions/merge-files')
+const config = require('../style.config')
+
+const argv = yargs(hideBin(process.argv)).argv
+
+const StyleDictionary = require('style-dictionary').extend(config(argv.theme))
 
 StyleDictionary.registerFormat({
   name: 'custom/format/system-ui-theme',
