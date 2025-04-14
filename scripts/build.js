6ae72ad98e5ee1842393360d4bc2550daef9830b
Thomas Digby <tom@atomlearning.co.uk>
2025-04-14 15:36:18 +0100

    feat: add automatic theme creation

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,6 @@
 const yargs = require('yargs')
+const path = require('node:path')
+const dree = require('dree')
 
 const formatterSystemUiThemeTypes = require('../src/formatters/system-ui-theme-types')
 const formatterSystemUiTheme = require('../src/formatters/system-ui-theme')
@@ -8,27 +10,59 @@ const config = require('../style.config')
 
 const argv = yargs(process.argv).argv
 
-const StyleDictionary = require('style-dictionary').extend(config(argv.theme))
+const buildTheme = (name, path) => {
+  console.log(`\n> Building ${name} theme`)
 
-StyleDictionary.registerFormat({
-  name: 'custom/format/system-ui-theme',
-  formatter: formatterSystemUiTheme
-})
+  const StyleDictionary = require('style-dictionary').extend(config(name, path))
 
-StyleDictionary.registerFormat({
-  name: 'custom/format/system-ui-theme-types',
-  formatter: formatterSystemUiThemeTypes
-})
+  StyleDictionary.registerFormat({
+    name: 'custom/format/system-ui-theme',
+    formatter: formatterSystemUiTheme
+  })
 
-StyleDictionary.registerFormat({
-  name: 'custom/format/scss-map-flat',
-  formatter: formatterScssMapFlat
-})
+  StyleDictionary.registerFormat({
+    name: 'custom/format/system-ui-theme-types',
+    formatter: formatterSystemUiThemeTypes
+  })
 
-StyleDictionary.registerAction({
-  name: 'merge-files',
-  do: actionMergeFiles,
-  undo: () => null
-})
+  StyleDictionary.registerFormat({
+    name: 'custom/format/scss-map-flat',
+    formatter: formatterScssMapFlat
+  })
 
-StyleDictionary.buildAllPlatforms()
+  StyleDictionary.registerAction({
+    name: 'merge-files',
+    do: actionMergeFiles,
+    undo: () => null
+  })
+
+  StyleDictionary.buildAllPlatforms()
+}
+
+const isDir = (child) => child.type === 'directory'
+
+const run = () => {
+  const { children } = dree.scan(path.resolve(process.cwd(), argv.path), {
+    size: false,
+    sizeInBytes: false,
+    hash: false,
+    stat: false
+  })
+
+  for (const dir of children) {
+    const name = dir.name
+
+    // build root theme
+    buildTheme(name)
+
+    if (dir.children.some(isDir)) {
+      for (const nestedDir of dir.children.filter(isDir)) {
+        const nestedName = nestedDir.name
+
+        buildTheme(`${name}-${nestedName}`, [name, nestedName])
+      }
+    }
+  }
+}
+
+run()
--- style.config.js
+++ style.config.js
@@ -1,55 +1,51 @@
-module.exports = (name) => ({
-  source: ['src/properties/**/*.json', name && `src/themes/${name}/**/*.json`],
-  platforms: {
-    scss: {
-      transformGroup: 'scss',
-      buildPath: 'lib/',
-      files: [
-        {
-          destination: '_variables.scss',
-          format: 'scss/variables',
-          filter: ({ attributes: { category } }) =>
-            category === 'color' || category === 'font'
-        },
-        {
-          destination: '_scales.scss',
-          format: 'custom/format/scss-map-flat',
-          // purposely omitting category === 'ratios'
-          // The ratio values include a slash (`/`) which is deprecated in the sass
-          // version we use. As sass is legacy for us and we are going to remove it, we
-          // decided that is not worth the hassle to make it work
-          filter: ({ attributes: { category } }) =>
-            category === 'size' || category === 'effects'
-        }
-      ],
-      name,
-      actions: ['merge-files']
-    },
-    js: {
-      transformGroup: 'js',
-      transforms: ['attribute/cti', 'name/cti/pascal', 'size/rem', 'color/hsl'],
-      buildPath: 'lib/',
-      files: [
-        {
-          destination: name ? `theme-${name}.js` : 'index.js',
-          format: 'custom/format/system-ui-theme'
-        }
-      ]
-    },
-    ts: {
-      transformGroup: 'js',
-      buildPath: 'lib/',
-      files: [
-        {
-          destination: name ? `theme-${name}.d.ts` : 'index.d.ts',
-          format: 'custom/format/system-ui-theme-types'
-        }
-      ]
-    },
-    'assets/copy': {
-      actions: ['copy_assets'],
-      buildPath: 'lib/',
-      transformGroup: 'assets'
+module.exports = (name, path = name) => {
+  const source = ['src/properties/**/*.json']
+
+  if (Array.isArray(path)) {
+    path.reduce((prev, curr) => {
+      source.push(`src/themes${prev}/${curr}/**/*.json`)
+      return `${prev}/${curr}`
+    }, '')
+  } else {
+    source.push(`src/themes/${path}/**/*.json`)
+  }
+
+  console.log(`Building theme from source: ${source}`)
+
+  return {
+    source,
+    platforms: {
+      js: {
+        transformGroup: 'js',
+        transforms: [
+          'attribute/cti',
+          'name/cti/pascal',
+          'size/rem',
+          'color/hsl'
+        ],
+        buildPath: 'lib/',
+        files: [
+          {
+            destination: name ? `theme-${name}.js` : 'index.js',
+            format: 'custom/format/system-ui-theme'
+          }
+        ]
+      },
+      ts: {
+        transformGroup: 'js',
+        buildPath: 'lib/',
+        files: [
+          {
+            destination: name ? `theme-${name}.d.ts` : 'index.d.ts',
+            format: 'custom/format/system-ui-theme-types'
+          }
+        ]
+      },
+      'assets/copy': {
+        actions: ['copy_assets'],
+        buildPath: 'lib/',
+        transformGroup: 'assets'
+      }
     }
   }
-})
+}
